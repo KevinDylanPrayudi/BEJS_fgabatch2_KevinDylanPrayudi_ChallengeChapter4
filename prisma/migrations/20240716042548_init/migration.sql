923ce3338e7ff25c33262243853d4e858511a229
KevinDylanPrayudi <70803233+KevinDylanPrayudi@users.noreply.github.com>
2024-07-17 09:48:30 +0700

    Revert "Transaction types"

--- prisma/migrations/20240716042548_init/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Accounts" ALTER COLUMN "created_at" DROP NOT NULL;
-
--- AlterTable
-ALTER TABLE "Transactions" ALTER COLUMN "date" DROP NOT NULL;
--- routes/index.js
+++
@@ -1,8 +0,0 @@
-const api = require('./api');
-
-function main(router, db) {
-    router.use('/api', api(db));
-    return router;
-}
-
-module.exports = main;
